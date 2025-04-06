061ddaac74624720b0f427782fff584801ef1ec5
DCD <DigitalCoolDude@hotmail.com>
2025-04-05 19:50:40 -0600

    bookbot project finish

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import sys
 from stats import get_num_words
 from stats import sort_dict
 from stats import get_num_char
 
 def main():
-    book_path ="books/frankenstein.txt" 
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path =sys.argv[1] 
     file_contents = get_book_text(book_path)
     print_report(book_path, file_contents)
 
